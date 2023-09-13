6c8238837639e3995fc98982afb95e928808b05c
xipian <xipian@foxmail.com>
2023-09-14 00:10:27 +0800

    third commit: finished basic backend 补充

---
+++ src/main/java/com/xipian/usercenter/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.xipian.usercenter.mapper;
+
+import com.xipian.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author NOBITA
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2023-09-13 21:10:30
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
