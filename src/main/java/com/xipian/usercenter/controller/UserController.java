cd6204cef4f4c593969db15de10f959663fc51ea
xipian <xipian@foxmail.com>
2023-09-16 18:48:28 +0800

    小修改

--- src/main/java/com/xipian/usercenter/controller/UserController.java
+++ src/main/java/com/xipian/usercenter/controller/UserController.java
@@ -23,48 +23,48 @@ import static com.xipian.usercenter.constant.userConstant.USER_LOGIN_STATE;
  */
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
 
     @Resource
     private UserService userService;
 
     @PostMapping("/register")
-    public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest){
-        if (userRegisterRequest == null){
+    public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
+        if (userRegisterRequest == null) {
             return null;
         }
         String userAccount = userRegisterRequest.getUserAccount();
         String userPassword = userRegisterRequest.getUserPassword();
         String checkPassword = userRegisterRequest.getCheckPassword();
-        if (StringUtils.isAnyBlank(userAccount,userPassword,checkPassword)){
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
             return null;
         }
-        return userService.userRegister(userAccount,userPassword,checkPassword);
+        return userService.userRegister(userAccount, userPassword, checkPassword);
 
     }
 
     @PostMapping("/login")
-    public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request){
-        if (userLoginRequest == null){
+    public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
+        if (userLoginRequest == null) {
             return null;
         }
         String userAccount = userLoginRequest.getUserAccount();
         String userPassword = userLoginRequest.getUserPassword();
-        if (StringUtils.isAnyBlank(userAccount,userPassword)){
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
             return null;
         }
-        return userService.userLogin(userAccount,userPassword,request);
+        return userService.userLogin(userAccount, userPassword, request);
     }
 
     @GetMapping("/search")
-    public List<User> searchUsers(String username,HttpServletRequest request){
-        if (!isAdmin(request)){
+    public List<User> searchUsers(String username, HttpServletRequest request) {
+        if (!isAdmin(request)) {
             return new ArrayList<>();
         }
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-        if (StringUtils.isNotBlank(username)){
-            userQueryWrapper.like("username",username);
+        if (StringUtils.isNotBlank(username)) {
+            userQueryWrapper.like("username", username);
         }
         List<User> userList = userService.list(userQueryWrapper);
         //将userList变成流遍历，脱敏信息后重新变为List
@@ -73,11 +73,11 @@ public class UserController {
     }
 
     @PostMapping("/delete")
-    public boolean deleteUser(@RequestBody long id,HttpServletRequest request){
-        if (!isAdmin(request)){
+    public boolean deleteUser(@RequestBody long id, HttpServletRequest request) {
+        if (!isAdmin(request)) {
             return false;
         }
-        if (id <= 0){
+        if (id <= 0) {
             return false;
         }
 
@@ -90,10 +90,10 @@ public class UserController {
      * @param request
      * @return
      */
-    private boolean isAdmin(HttpServletRequest request){
+    private boolean isAdmin(HttpServletRequest request) {
         Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
         User user = (User) userObj;
-        if (user == null || user.getUserRole() != ADMIN_ROLE){
+        if (user == null || user.getUserRole() != ADMIN_ROLE) {
             return false;
         }
         return true;
--- src/test/java/com/xipian/usercenter/service/UserServiceTest.java
+++ src/test/java/com/xipian/usercenter/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.xipian.usercenter.service;
 
+import com.xipian.usercenter.model.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
